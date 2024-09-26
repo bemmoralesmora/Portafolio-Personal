0101f35948f904ca3f5e4d41115c520da1c7c161
Brian Emanuel Morales Mora <bemorales@scl.edu.gt>
2024-09-26 12:07:16 -0600

    se agrega el elmento de data

--- componentes/proyect/proyect.js
+++ componentes/proyect/proyect.js
@@ -1,20 +1,48 @@
 import { header } from "../header/header.js";
-function proyectos (){
+import { dataproyectos } from "../../data/data.js";
+
+function proyectos() {
     let section = document.createElement('section');
     section.className = 'section-header';
     section.appendChild(header());
-    section.appendChild(item())
-
+    section.appendChild(listaProyectos());
 
     return section;
 }
 
-export {proyectos};
+export { proyectos };
 
+function listaProyectos() { 
+    let div3 = document.createElement('div');
+    div3.className = "lista-proyectos";
 
+    dataproyectos.forEach((proyecto) => {
+        div3.appendChild(item(proyecto.nombre, proyecto.descripcion, proyecto.web, ));
+    });
+    return div3;
+}
 
-function item(){
+function item(nombre, descripcion, githubpage, github, stacks) {
     let div2 = document.createElement('div');
-    div2.innerText = "item de lista"
+
+    let a = document.createElement('a');
+    a.className = "link-githubpage"
+    a.href = "https://bemmoralesmora.github.io/Proyecto-formulario/";
+    a.innerText = "PF-githubpage";
+    div2.appendChild(a);
+
+    let stacks1 = document.createElement('div');
+    stacks1.innerText = "js, css, html";
+    div2.appendChild(stacks1);
+
+    let btnDiv = document.createElement('a');
+    btnDiv.className = "btn-github";
+    btnDiv.href = "https://github.com/bemmoralesmora/Proyecto-formulario";
+    btnDiv.innerText = "github"
+    div2.appendChild(btnDiv);
+
+
+    div2.className = "div-item"
+
     return div2;
 }
