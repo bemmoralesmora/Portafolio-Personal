42f205883b1f0afa870ee485a9c8f1a15d2aae98
Brian Emanuel Morales Mora <bemorales@scl.edu.gt>
2024-09-26 10:06:39 -0600

    segunda version de portafolio - se agrega el header -se agrega un elemento item de guia

--- componentes/proyect/proyect.js
+++ componentes/proyect/proyect.js
@@ -1,3 +1,4 @@
+import { header } from "../header/header.js";
 function proyectos (){
     let section = document.createElement('section');
     section.className = 'section-header';
@@ -10,20 +11,7 @@ function proyectos (){
 
 export {proyectos};
 
-function header(){
-    let div = document.createElement('div');
-    div.className = 'header';
 
-    let h2 = document.createElement('h2');
-    h2.innerHTML = "proyectos";
-    div.appendChild(h2);
-
-    let btn = document.createElement('div');
-    btn.innerHTML= "github";
-    div.appendChild(btn);
-
-    return div;
-}
 
 function item(){
     let div2 = document.createElement('div');
