7c28b9536b73064e2ab2604fdb3e821d5795d395
Brian Emanuel Morales Mora <bemorales@scl.edu.gt>
2024-10-09 13:54:56 -0600

    Se integran funciones en item

--- componentes/proyect/proyect.js
+++ componentes/proyect/proyect.js
@@ -17,32 +17,45 @@ function listaProyectos() {
     div3.className = "lista-proyectos";
 
     dataproyectos.forEach((proyecto) => {
-        div3.appendChild(item(proyecto.nombre, proyecto.descripcion, proyecto.web, ));
+        div3.appendChild(item(proyecto.nombre, proyecto.desarrolladores, proyecto.fecha, proyecto.github, proyecto.stacks, proyecto.descargas ));
     });
     return div3;
 }
 
-function item(nombre, descripcion, githubpage, github, stacks) {
+function item(nombre, desarrolladores, fecha, github, stacks, descargas) {
     let div2 = document.createElement('div');
 
     let a = document.createElement('a');
-    a.className = "link-githubpage"
-    a.href = "https://bemmoralesmora.github.io/Proyecto-formulario/";
-    a.innerText = "PF-githubpage";
+    a.className = "link-githubpage";
+    a.href = github; 
+    a.innerText = nombre; 
     div2.appendChild(a);
 
     let stacks1 = document.createElement('div');
-    stacks1.innerText = "js, css, html";
+    stacks1.innerText = stacks.join(', ');  
     div2.appendChild(stacks1);
+
+    let personas = document.createElement('div');
+    personas.innerText = desarrolladores;  
+    div2.appendChild(personas);
+
+    let fecha1 = document.createElement('div');
+    fecha1.innerText = fecha; 
+    div2.appendChild(fecha1);
 
     let btnDiv = document.createElement('a');
     btnDiv.className = "btn-github";
-    btnDiv.href = "https://github.com/bemmoralesmora/Proyecto-formulario";
-    btnDiv.innerText = "github"
+    btnDiv.href = github;  
+    btnDiv.innerText = "GitHub";
     div2.appendChild(btnDiv);
 
+    let btndescargar = document.createElement("a");
+    btndescargar.className = "descargar";
+    btndescargar.href = descargas;  
+    btndescargar.innerText = "Descargar";
+    div2.appendChild(btndescargar);
 
-    div2.className = "div-item"
+    div2.className = "div-item";
 
     return div2;
 }
